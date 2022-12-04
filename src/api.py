d4acfc722ad84d95b1cd6e081f3145f795788316
seun-beta <adegokeseunfunmi1999@gmail.com>
2022-12-04 20:20:12 +0100

    chore: write route to test db creation
    Signed-off-by: seun-beta <adegokeseunfunmi1999@gmail.com>

--- src/api.py
+++ src/api.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify
 
 from src.constants.http_status_codes import HTTP_200_OK
+from src.models import Species
 
 mol_bp = Blueprint("mol", __name__, url_prefix="/api/v1/mol")
 
@@ -8,3 +9,10 @@ mol_bp = Blueprint("mol", __name__, url_prefix="/api/v1/mol")
 @mol_bp.get("/")
 def main():
     return jsonify({"id": "name"}, HTTP_200_OK)
+
+
+@mol_bp.get("/db")
+def db():
+    data = Species.query.all()
+
+    return jsonify({"id": data}, HTTP_200_OK)
